d4b72edc074cca79dd50b30526ce626ef089a82a
oyeitsmeow <zhongzichenn@gmail.com>
2024-08-31 19:49:04 +0800

    chore(*): 新增 git 及样式等配置

---
+++ .commitlintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ['cz'],
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-leading-blank': [2, 'always'],
+    'footer-max-line-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 100],
+    'subject-empty': [2, 'never'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      ['chore', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'wip'],
+    ],
+  },
+};
