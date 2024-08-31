01b52d427bdf0c35f857cb6499f52fcc566cf0bf
oyeitsmeow <zhongzichenn@gmail.com>
2024-08-31 20:44:33 +0800

    chore(*): 新增 husky 配置

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     'footer-leading-blank': [2, 'always'],
     'footer-max-line-length': [2, 'always', 100],
     'header-max-length': [2, 'always', 100],
+    // 'scope-case': [2, 'always', 'lower-case'],括号内小写还是太严格了，有可能要填写一些变量名
     'subject-empty': [2, 'never'],
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
